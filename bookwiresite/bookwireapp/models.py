9e385b628050748ffc7ddabb7b4d002100aaeb28
Cedric Kim <hello@cedric.today>
2020-01-25 13:05:34 +0100

    Create Book model

--- bookwiresite/bookwireapp/models.py
+++ bookwiresite/bookwireapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.TextField()
+    author = models.TextField(blank=True)
+    description = models.TextField(blank=True)
+    ownerUsername = models.TextField()
+    borrowerUsername = models.TextField(blank=True)
+    datepublished = models.DateField(blank=True)
+    isbnthirteen = models.TextField()
+    publisher = models.TextField()
+    datelastmodified = models.DateTimeField(auto_now=True)
